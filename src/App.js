8dfad3d0935fccf7705b4d296607a33c78e3cc0a
sd08051sd08051 <southafrica.2018.durban@gmail.com>
2023-11-13 19:50:38 +0900

    1st

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {
+  Container,
+  Form,
+  InputGroup,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+// import { Button } from "bootstrap";
+// import { InputGroupText } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <h1 className="mt-4">Todo リスト</h1>
+        <Form>
+          <InputGroup>
+            <FormControl type="text" value="入力してね" />
+            <InputGroup.Text addonType="append">
+              <Button type="submit">追加</Button>
+            </InputGroup.Text>
+          </InputGroup>
+        </Form>
+      </Container>
     </div>
   );
 }
